2d8e15b1d2eee1377fdbc5af6388387da8b0fc9c
xdzi <1055671428@qq.com>
2019-06-19 23:58:06 +0800

    提交第一个TS示例

---
+++ examples/hello.js
@@ -0,0 +1,5 @@
+function hello(name) {
+    return 'Hello ' + name;
+}
+var user = 'word';
+console.log(hello(user));
---
+++ examples/hello.ts
@@ -0,0 +1,6 @@
+function hello(name) {
+    return 'Hello ' + name
+}
+let user = 'word'
+
+console.log(hello(user))
